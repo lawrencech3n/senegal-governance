b5f0725b534b9d9576f74b20a60ab4dab7200d42
Lawrence Chen <146892035+lawrencech3n@users.noreply.github.com>
2026-05-10 23:06:04 -0700

    Update extraction.ts

--- lib/data/extraction.ts
+++ lib/data/extraction.ts
@@ -1,3 +1,24 @@
-export const extractionByYear = [];
-export const exportDestinations2020 = [];
-export const majorOperators = [];
+export const extractionByYear = [
+  {
+    year: "2020",
+    phosphates: 1,
+    gold: 1,
+    zircon: 1,
+  },
+];
+
+export const exportDestinations2020 = [
+  {
+    country: "France",
+    value: 1,
+  },
+];
+
+export const majorOperators = [
+  {
+    name: "Placeholder operator",
+    sector: "Mining",
+    origin: "France",
+    role: "Placeholder role",
+  },
+];
