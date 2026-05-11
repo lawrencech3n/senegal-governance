19b626e36398314aaab64fef29c1be37b9e17425
Lawrence Chen <146892035+lawrencech3n@users.noreply.github.com>
2026-05-10 23:00:35 -0700

    Update environment.ts

--- lib/data/environment.ts
+++ lib/data/environment.ts
@@ -1,2 +1,14 @@
-export const envIndicators = [];
-export const environmentalImpacts = [];
+export const envIndicators = [
+  {
+    year: "2020",
+    value: 1,
+  },
+];
+
+export const environmentalImpacts = [
+  {
+    title: "Environmental Impact",
+    category: "Placeholder category",
+    body: "Placeholder body text.",
+  },
+];
